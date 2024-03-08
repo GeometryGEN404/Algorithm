d42476d945a080e7db602dd01ef3868b84e9260f
MrL <cqmrl@MrLdeMacBook-Pro.local>
2024-03-08 20:32:46 +0800

    二分查找

---
+++ 数组/二分查找.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int>nums(6,0);
+    nums={-1,0,3,5,9,12};
+    int target=9;
+    cout<<s.search(nums,target);
+}
